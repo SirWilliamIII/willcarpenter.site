de7f8b15b1bbd19ac50be92303aede1eddb548b8
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-06-28 15:02:08 -0500

    added styling

--- js/script.js
+++ js/script.js
@@ -7,112 +7,108 @@ document.addEventListener("DOMContentLoaded", () => {
 
         setTimeout(() => {
             intro.classList.add("hidden");
+            console.log("hello");
             content.classList.remove("hidden");
             content.classList.add("visible");
         }, 1000);
     }, 5000);
 
     /*============================================
-    Page Preloader
-    ==============================================*/
-
+      Page Preloader
+      ==============================================*/
 
     /*============================================
-    Header
-    ==============================================*/
+      Header
+      ==============================================*/
 
     // document.getElementById('home').style.height = `${window.innerHeight + 50}px`;
 
-    window.addEventListener('scroll', function() {
+    window.addEventListener("scroll", function() {
         var st = window.pageYOffset || document.documentElement.scrollTop;
         var wh = window.innerHeight;
         var sf = 1 + st / (10 * wh);
 
-        var backstretchImg = document.querySelector('.backstretch img');
+        var backstretchImg = document.querySelector(".backstretch img");
         if (backstretchImg) {
-            backstretchImg.style.transform = 'scale(' + sf + ')';
-            backstretchImg.style.webkitTransform = 'scale(' + sf + ')';
+            backstretchImg.style.transform = "scale(" + sf + ")";
+            backstretchImg.style.webkitTransform = "scale(" + sf + ")";
         }
 
-        var homeContainer = document.querySelector('#home .container');
+        var homeContainer = document.querySelector("#home .container");
         if (homeContainer) {
-            homeContainer.style.opacity = (1.6 - st / 400);
+            homeContainer.style.opacity = 1.6 - st / 400;
         }
 
-        var backstretch = document.querySelector('.backstretch');
+        var backstretch = document.querySelector(".backstretch");
         if (backstretch) {
-            if (window.pageYOffset > (window.innerHeight + 50)) {
-                backstretch.style.display = 'none';
+            if (window.pageYOffset > window.innerHeight + 50) {
+                backstretch.style.display = "none";
             } else {
-                backstretch.style.display = 'block';
+                backstretch.style.display = "block";
             }
         }
     });
 
     /*============================================
-    Navigation Bar
-    ==============================================*/
-    if (window.pageYOffset < (window.innerHeight - 35)) {
-        document.getElementById('main-nav').classList.remove('scrolled');
+      Navigation Bar
+      ==============================================*/
+    if (window.pageYOffset < window.innerHeight - 35) {
+        document.getElementById("main-nav").classList.remove("scrolled");
     } else {
-        document.getElementById('main-nav').classList.add('scrolled');
+        document.getElementById("main-nav").classList.add("scrolled");
     }
 
-    window.addEventListener('scroll', function() {
-        if (window.pageYOffset < (window.innerHeight - 35)) {
-            document.getElementById('main-nav').classList.remove('scrolled');
+    window.addEventListener("scroll", function() {
+        if (window.pageYOffset < window.innerHeight - 35) {
+            document.getElementById("main-nav").classList.remove("scrolled");
         } else {
-            document.getElementById('main-nav').classList.add('scrolled');
+            document.getElementById("main-nav").classList.add("scrolled");
         }
     });
 
     document.querySelectorAll("#main-nav a").forEach(function(element) {
-        element.addEventListener('click', function(event) {
-            console.log(event.target)
-            var target = ''
-            if(document.querySelector(this.getAttribute('href'))) {
-                target = document.querySelector(this.getAttribute('href'));
+        element.addEventListener("click", function(event) {
+            console.log(event.target);
+            var target = "";
+            if (document.querySelector(this.getAttribute("href"))) {
+                target = document.querySelector(this.getAttribute("href"));
             }
-
 
             window.scrollTo({
                 top: target.offsetTop - 30,
-                behavior: 'smooth'
+                behavior: "smooth",
             });
-            if (window.innerWidth < 769 && document.getElementById('site-nav').offsetWidth > 0) {
-                document.querySelector('.navbar-toggle').click();
+            if (
+                window.innerWidth < 769 &&
+                document.getElementById("site-nav").offsetWidth > 0
+            ) {
+                document.querySelector(".navbar-toggle").click();
             }
         });
     });
 
-
     /*============================================
-    Project thumbs
-    ==============================================*/
+      Project thumbs
+      ==============================================*/
     $(window).load(function() {
-
-        $('#projects-container').css({
-            visibility: 'visible'
+        $("#projects-container").css({
+            visibility: "visible",
         });
 
-        $('#projects-container').masonry({
-            itemSelector: '.project-item:not(.filtered)',
+        $("#projects-container").masonry({
+            itemSelector: ".project-item:not(.filtered)",
             columnWidth: 350,
             isFitWidth: true,
             isResizable: true,
             isAnimated: !Modernizr.csstransitions,
-            gutterWidth: 25
+            gutterWidth: 25,
         });
-
     });
 
     /*============================================
 
 
-    /*============================================
-    Contact Map
-    ==============================================*/
-
-
-
+      /*============================================
+      Contact Map
+      ==============================================*/
 });
