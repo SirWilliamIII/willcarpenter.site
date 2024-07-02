8b81ad7ca23ff995af846e54ee282c8febaa0e5e
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-07-02 12:56:27 -0500

    updates to package.json, imported path module

--- server/app.js
+++ server/app.js
@@ -2,18 +2,16 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-const PORT = process.env.PORT || 3000
-// Example usage of __dirname
-app.use(
-  express.static(
-    path.join("/Users/sirwilliam/Programming/vanilla-resume", "public")
-  )
-);
+const PORT = process.env.PORT || 3000;
+
+// Navigate up one level from the current directory and then into the "public" directory
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 app.get("/", function (_, res) {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  // Similarly, navigate up and then into the "public/index.html" file
+  res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(PORT, function () {
-  console.log(`Server is running on port ${ PORT }`);
+  console.log(`Server is running on port ${PORT}`);
 });
